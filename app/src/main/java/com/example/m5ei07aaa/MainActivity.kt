fc489122bf3bb3c0eb9d72a629a48ae893727831
AlfredCode <alcodevsc@gmail.com>
2024-10-08 22:14:17 -0300

    hola

--- app/src/main/java/com/example/m5ei07aaa/MainActivity.kt
+++ app/src/main/java/com/example/m5ei07aaa/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.m5ei07aaa
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                MainView()
+            }
+        }
+    }
+}
 
 @Composable
 fun MainView() {
--- app/src/main/java/com/example/m5ei07aaa/ManejoImagen.kt
+++ app/src/main/java/com/example/m5ei07aaa/ManejoImagen.kt
@@ -2,21 +2,25 @@ package com.example.m5ei07aaa
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun BotonImagen() {
     var ImagenVisible by remember { mutableStateOf(false) }
-    var boton01 by remember { mutableStateOf("Mostrar Imagen") }
+    var boton01 by remember { mutableStateOf("Mostrar Imágenes") }
 
     Button(onClick = {
         ImagenVisible = !ImagenVisible
-        boton01 = if (ImagenVisible) "Ocultar Imagen" else "Mostrar Imagen"
+        boton01 = if (ImagenVisible) "Ocultdsar Imágefdnes" else "Mostrar Imágenes"
     }) {
         Text(text = boton01)
     }
@@ -24,10 +28,38 @@ fun BotonImagen() {
     Spacer(modifier = Modifier.height(16.dp))
 
     if (ImagenVisible) {
-        Image(
-            painter = painterResource(id = R.drawable.a),
-            contentDescription = "Imagen de la app",
-            modifier = Modifier.size(200.dp)
+        // Lista de recursos de imágenes
+        val imagenes = listOf(
+            R.drawable.a01,
+            R.drawable.a02,
+            R.drawable.a03,
+            R.drawable.a04,
+            R.drawable.a05
         )
+
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp) // Espacio entre las imágenes
+        ) {
+            items(imagenes) { imagenRes ->
+                Image(
+                    painter = painterResource(id = imagenRes),
+                    contentDescription = "Imagen de la app",
+                    modifier = Modifier.size(200.dp)
+                )
+            }
+
+        }
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center
+        ) {
+            items(imagenes) { imagenRes ->
+                Image(
+                    painter = painterResource(id = imagenRes),
+                    contentDescription = "Imagen de la app",
+                    modifier = Modifier.size(200.dp)
+                )
+            }
     }
+}
 }
